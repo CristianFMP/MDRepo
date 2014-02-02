ee55faa16ef382904d08349e15083b205f9078c4
valeriaalampi <valeria.alampi@gmail.com>
2014-02-02 13:53:35 +0100

    Tolto l'autoincrement al campo _id della tabella Itinerari, a favoro della chiave multipla (_id+nicknameUtente); perchè insieme le due cosse causavano problemi in sqlite. Sarà da incrementare _id da codice.

--- SmartItineraryClient/src/md/smartitineraryclient/database/SISQLiteHelper.java
+++ SmartItineraryClient/src/md/smartitineraryclient/database/SISQLiteHelper.java
@@ -8,6 +8,7 @@ import android.util.Log;
 public class SISQLiteHelper extends SQLiteOpenHelper {
 	
 	/** METADATI DELLE TABELLE */
+	// Itinerari
 	public static final String ITIN_TABLE = "Itinerari";
   	public static final String ITIN_COLUMN_ID = "_id";
   	public static final String ITIN_COLUMN_NICKNAME_UTENTE = "nicknameUtente";
@@ -16,22 +17,29 @@ public class SISQLiteHelper extends SQLiteOpenHelper {
   	public static final String ITIN_COLUMN_LUNGHEZZA = "lunghezza";
   	public static final String ITIN_COLUMN_N_POI = "numPOI";
   	
+  	// Preferenze
+  	public static final String PREF_TABLE = "Preferenze";
+	public static final String PREF_COLUMN_ID_ITIN = "_idItinerario";
+	public static final String PREF_COLUMN_NICKNAME_UTENTE = "nicknameUtente";
+	public static final String PREF_COLUMN_POS_UTENTE = "posizioneUtente";
+	public static final String PREF_COLUMN_DATETIME = "datetime";
+  	//...
   	private static final String DATABASE_NAME = "smartitineraryDB.db";
     private static final int DATABASE_VERSION = 1;
   
 
     /** CODICE SQL DI CREAZIONE DELLE TABELLE */
-	private static final String ITIN_TABLE_CREATE = "CREATE TABLE IF NOT EXISTS "  // Itinerari
-  		+ ITIN_TABLE + " (" 
-  		+ ITIN_COLUMN_ID + " integer primary key autoincrement, "
-  		+ ITIN_COLUMN_NICKNAME_UTENTE + " text not null, "
-  		+ ITIN_COLUMN_ELENCO_POI + " text not null, "
-  		+ ITIN_COLUMN_POPOLARITA + " integer not null, "
-  		+ ITIN_COLUMN_LUNGHEZZA + " double not null, "
-  		+ ITIN_COLUMN_N_POI + " integer not null "
-  		//+ "primary key(" + ITIN_COLUMN_ID + ", " + ITIN_COLUMN_NICKNAME_UTENTE + ")"
-  		+ ");";
-	
+    private static final String ITIN_TABLE_CREATE = "CREATE TABLE IF NOT EXISTS "  // Itinerari
+      		+ ITIN_TABLE + " (" 
+      		+ ITIN_COLUMN_ID + " integer not null, "
+      		+ ITIN_COLUMN_NICKNAME_UTENTE + " text not null, "
+      		+ ITIN_COLUMN_ELENCO_POI + " text not null, "
+      		+ ITIN_COLUMN_POPOLARITA + " integer not null, "
+      		+ ITIN_COLUMN_LUNGHEZZA + " double not null, "
+      		+ ITIN_COLUMN_N_POI + " integer not null, "
+      		+ "primary key(" + ITIN_COLUMN_ID + ", " + ITIN_COLUMN_NICKNAME_UTENTE + ")"
+      		+ ");";
+    
 
 	public SISQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -40,6 +48,7 @@ public class SISQLiteHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase database) {
 		database.execSQL(ITIN_TABLE_CREATE);
+		System.out.println(ITIN_TABLE_CREATE);
 	}
 
   @Override
