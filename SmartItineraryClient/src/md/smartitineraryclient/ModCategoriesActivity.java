7d55d91172406bed29fbafb10fdf085b8faf5124
Cristian Fompowou <cristian.fmp@gmail.com>
2014-02-09 22:09:28 +0100

    procede lavoro su modcategoriesactivity

--- SmartItineraryClient/src/md/smartitineraryclient/ModCategoriesActivity.java
+++ SmartItineraryClient/src/md/smartitineraryclient/ModCategoriesActivity.java
@@ -1,13 +1,37 @@
 package md.smartitineraryclient;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.Toast;
 
 public class ModCategoriesActivity extends Activity {
+	private static final String SERVICE_URL = "http://192.168.0.18:8080/SmartItineraryWebService/rest/comment";
+	private static final String TAG = "ModCategoriesActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +39,23 @@ public class ModCategoriesActivity extends Activity {
 		setContentView(R.layout.activity_mod_categories);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		String url = SERVICE_URL + "/getCategories";
+		WebServiceTask wst = new WebServiceTask(WebServiceTask.GET_TASK, this, "Retrieving Categories...");
+		// show toast with real url
+		Context context = getApplicationContext();
+		CharSequence text = url;
+		int duration = Toast.LENGTH_LONG;
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+		// execute the call
+		wst.execute(new String[] { url });
 	}
 
 	/**
 	 * Set up the {@link android.app.ActionBar}.
 	 */
 	private void setupActionBar() {
-
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-
 	}
 
 	@Override
@@ -59,5 +91,121 @@ public class ModCategoriesActivity extends Activity {
 	    startActivity(intent);
 	    overridePendingTransition(0,0);
 	}
+	
+	public void handleResponse(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private class WebServiceTask extends AsyncTask<String, Integer, String> {
+		public static final int POST_TASK = 1;
+		public static final int GET_TASK = 2;	
+		private static final String TAG = "WebServiceTask";
+		// connection timeout, in milliseconds (waiting to connect)
+		private static final int CONN_TIMEOUT = 3000;
+		// socket timeout, in milliseconds (waiting for data)
+		private static final int SOCKET_TIMEOUT = 50000;
+		private int taskType = GET_TASK;
+		private Context mContext = null;
+		private String processMessage = "Processing...";
 
+		// params are used only for UrlEncoded POST requests
+		private ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+
+		private ProgressDialog pDlg = null;
+
+		public WebServiceTask(int taskType, Context mContext, String processMessage) {
+			this.taskType = taskType;
+			this.mContext = mContext;
+			this.processMessage = processMessage;
+		}
+		
+		private void showProgressDialog() {
+			pDlg = new ProgressDialog(mContext);
+			pDlg.setMessage(processMessage);
+			pDlg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			pDlg.setCancelable(false);
+			pDlg.show();
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			showProgressDialog();
+		}
+		
+		@Override
+		protected String doInBackground(String... params) {
+			String url = params[0];
+			String result = "";
+			HttpResponse response = doResponse(url);
+			if (response == null) {
+				return result;
+			} else {
+				try {
+					result = inputStreamToString(response.getEntity().getContent());
+				} catch (IllegalStateException e) {
+					Log.e(TAG, e.getLocalizedMessage(), e);
+				} catch (IOException e) {
+					Log.e(TAG, e.getLocalizedMessage(), e);
+				}
+			}
+			return result;
+		}
+		
+		protected void onPostExecute(String response) {
+			handleResponse(response);
+			pDlg.dismiss();
+		}
+
+		private String inputStreamToString(InputStream content) {
+			String line = "";
+			StringBuilder total = new StringBuilder();
+
+			// Wrap a BufferedReader around the InputStream
+			BufferedReader rd = new BufferedReader(new InputStreamReader(content));
+
+			try {
+				// Read response until the end
+				while ((line = rd.readLine()) != null) {
+					total.append(line);
+				}
+			} catch (IOException e) {
+				Log.e(TAG, e.getLocalizedMessage(), e);
+			}
+			// Return full string
+			return total.toString();
+		}
+
+		private HttpResponse doResponse(String url) {
+			// Use our connection and data timeouts as parameters for our
+			// DefaultHttpClient
+			HttpClient httpclient = new DefaultHttpClient(getHttpParams());
+			HttpResponse response = null;
+
+			try {
+				switch (taskType) {
+					case POST_TASK:
+						HttpPost httppost = new HttpPost(url);
+						// Add parameters
+						httppost.setEntity(new UrlEncodedFormEntity(params));
+						response = httpclient.execute(httppost);
+						break;
+					case GET_TASK:
+						HttpGet httpget = new HttpGet(url);
+						response = httpclient.execute(httpget);
+						break;
+				}
+			} catch (Exception e) {
+				Log.e(TAG, e.getLocalizedMessage(), e);
+			}
+			return response;
+		}
+
+		private HttpParams getHttpParams() {
+			HttpParams htpp = new BasicHttpParams();
+			HttpConnectionParams.setConnectionTimeout(htpp, CONN_TIMEOUT);
+			HttpConnectionParams.setSoTimeout(htpp, SOCKET_TIMEOUT);
+			return htpp;
+		}		
+	}
 }
