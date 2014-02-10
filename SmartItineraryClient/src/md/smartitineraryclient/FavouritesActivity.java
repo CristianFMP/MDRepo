f9320fd25b62abfe348439d9008b19542ece0d03
valeriaalampi <valeria.alampi@gmail.com>
2014-02-10 20:47:56 +0100

    Alcune modifiche favourites e mod_itin

--- SmartItineraryClient/src/md/smartitineraryclient/FavouritesActivity.java
+++ SmartItineraryClient/src/md/smartitineraryclient/FavouritesActivity.java
@@ -12,22 +12,16 @@ public class FavouritesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_favourites);
-		// Show the Up button in the action bar.
 		setupActionBar();
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
+
 	private void setupActionBar() {
-
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.favourites, menu);
 		return true;
 	}
@@ -36,13 +30,6 @@ public class FavouritesActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			overridePendingTransition(0,0);
 			return true;
